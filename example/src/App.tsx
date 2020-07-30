6944322cccf8fd9e501388ce267842c4ecbf8e22
xcncopar@hants.gov.uk <xcncopar@hants.gov.uk>
2020-07-30 21:51:11 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'bundle-all'
+import 'bundle-all/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
