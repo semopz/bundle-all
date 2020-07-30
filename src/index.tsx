8bcd916829381f17809679a9175bcd004b7a43a6
xcncopar@hants.gov.uk <xcncopar@hants.gov.uk>
2020-07-30 22:33:27 +0100

    Added Mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import styles from './styles.module.css'
-
+import Button from '@material-ui/core/Button'
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return (
+    <div className={styles.test}>
+      Example Component: {text}
+      <Button>Click me</Button>
+    </div>
+  )
 }
